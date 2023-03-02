e536480db5085474359d00ece536c3d3c6195acb
mxz <60683892+mxzLV@users.noreply.github.com>
2023-03-03 04:45:43 +0700

    add post for subject in semester

--- admin.php
+++ admin.php
@@ -1,12 +1,13 @@
 <?php
 // Bảo vệ tài khoản khi chưa đăng nhập vẫn nhảy vào trang
-if(!defined("TEMPLATE")){
+if (!defined("TEMPLATE")) {
 	die("You do not have permission to access this file !");
 }
 // Sử dụng để khi nào cần gọi tới trang nào thì import vào
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -20,20 +21,22 @@ if(!defined("TEMPLATE")){
 		/* Chrome, Safari, Edge, Opera */
 		input::-webkit-outer-spin-button,
 		input::-webkit-inner-spin-button {
-		  -webkit-appearance: none;
-		  margin: 0;
+			-webkit-appearance: none;
+			margin: 0;
 		}
 
 		/* Firefox */
 		input[type=number] {
-		  -moz-appearance: textfield;
+			-moz-appearance: textfield;
 		}
+
 		table {
-		    counter-reset: tableCount;     
+			counter-reset: tableCount;
 		}
-		.counterCell:before {              
-		    content: counter(tableCount); 
-		    counter-increment: tableCount; 
+
+		.counterCell:before {
+			content: counter(tableCount);
+			counter-increment: tableCount;
 		}
 	</style>
 </head>
@@ -54,7 +57,7 @@ if(!defined("TEMPLATE")){
 						<a href="#" class="dropdown-toggle" data-toggle="dropdown"><svg class="glyph stroked male-user">
 								<use xlink:href="#stroked-male-user"></use>
 							</svg> Admin Manager</a>
-				
+
 					</li>
 				</ul>
 			</div>
@@ -62,11 +65,11 @@ if(!defined("TEMPLATE")){
 		</div><!-- /.container-fluid -->
 	</nav>
 
-	<?php 
-		$directoryURI = $_SERVER['REQUEST_URI'];
-		$path = parse_url($directoryURI, PHP_URL_PATH);
-		$components = explode('/', $path);
-		$first_part = $components[1];
+	<?php
+	$directoryURI = $_SERVER['REQUEST_URI'];
+	$path = parse_url($directoryURI, PHP_URL_PATH);
+	$components = explode('/', $path);
+	$first_part = $components[1];
 	?>
 
 	<div id="sidebar-collapse" class="col-sm-3 col-lg-2 sidebar">
@@ -76,28 +79,47 @@ if(!defined("TEMPLATE")){
 			</div>
 		</form>
 		<ul class="nav menu">
-			<li class="active"><a href="index.php"><svg class="glyph stroked app window with content"><use xlink:href="#stroked-app-window-with-content"/></svg>Trang chủ</a></li>
+			<li class="active"><a href="index.php"><svg class="glyph stroked app window with content">
+						<use xlink:href="#stroked-app-window-with-content" />
+					</svg>Trang chủ</a></li>
 
-			<li><a href="index.php?page_layout=class"><svg class="glyph stroked home"><use xlink:href="#stroked-home"/></svg>Class</a></li>
+			<li><a href="index.php?page_layout=class"><svg class="glyph stroked home">
+						<use xlink:href="#stroked-home" />
+					</svg>Class</a></li>
 
-			<li><a href="index.php?page_layout=student"><svg class="glyph stroked male user"><use xlink:href="#stroked-male-user"/></svg>Student</a></li>
+			<li><a href="index.php?page_layout=student"><svg class="glyph stroked male user">
+						<use xlink:href="#stroked-male-user" />
+					</svg>Student</a></li>
 
-			<li><a href="index.php?page_layout=semester"><svg class="glyph stroked notepad "><use xlink:href="#stroked-notepad"/></svg>Semester</a></li>
+			<li><a href="index.php?page_layout=semester"><svg class="glyph stroked notepad ">
+						<use xlink:href="#stroked-notepad" />
+					</svg>Semester</a></li>
 
 
-			<li><a href="index.php?page_layout=subject"><svg class="glyph stroked clipboard with paper"><use xlink:href="#stroked-clipboard-with-paper"/></svg>Subject</a></li>
+			<li><a href="index.php?page_layout=subject"><svg class="glyph stroked clipboard with paper">
+						<use xlink:href="#stroked-clipboard-with-paper" />
+					</svg>Subject</a></li>
 
 			<li><a href="index.php?page_layout=point"><svg class="glyph stroked star">
-				<use xlink:href="#stroked-star"/></svg>Point</a></li>
+						<use xlink:href="#stroked-star" />
+					</svg>Point</a></li>
 
-			<li><a href="index.php?page_layout=teacher"><svg class="glyph stroked female user"><use xlink:href="#stroked-female-user"/></svg>Teacher</a></li>
+			<li><a href="index.php?page_layout=teacher"><svg class="glyph stroked female user">
+						<use xlink:href="#stroked-female-user" />
+					</svg>Teacher</a></li>
 
-			<li><a href="index.php?page_layout=schedue"><svg class="glyph stroked table"><use xlink:href="#stroked-table"/></svg>Schedue</a></li>
+			<li><a href="index.php?page_layout=schedue"><svg class="glyph stroked table">
+						<use xlink:href="#stroked-table" />
+					</svg>Schedue</a></li>
 
-			<li><a href="index.php?page_layout=user"><svg class="glyph stroked key "><use xlink:href="#stroked-key"/></svg>Admin</a></li>
+			<li><a href="index.php?page_layout=user"><svg class="glyph stroked key ">
+						<use xlink:href="#stroked-key" />
+					</svg>Admin</a></li>
 			<hr>
 			<li><a href="logout.php">
-				<svg class="glyph stroked male user "><use xlink:href="#stroked-male-user"/></svg>Logout</a></li>
+					<svg class="glyph stroked male user ">
+						<use xlink:href="#stroked-male-user" />
+					</svg>Logout</a></li>
 		</ul>
 
 	</div>
@@ -109,77 +131,100 @@ if(!defined("TEMPLATE")){
 	if (isset($_GET["page_layout"])) {
 		switch ($_GET["page_layout"]) {
 
-			case "teacher": include_once("teacher.php");
-			break;
-			case "add_teacher": include_once("add_teacher.php");
-			break;
-			case "edit_teacher": include_once("edit_teacher.php");
-			break;
-
-			case "subject": include_once("subject.php");
-			break;
-			case "add_subject": include_once("add_subject.php");
-			break;
-
-			case "class": include_once("class.php");
-			break;
-			case "add_class": include_once("add_class.php");
-			break;
-			case "edit_class": include_once("edit_class.php");
-			break;
-
-			case "semester": include_once("semester.php");
-			break;
-			case "add_semester": include_once("add_semester.php");
-			break;
-
-			case "point": include_once("point.php");
+			case "teacher":
+				include_once("teacher.php");
 				break;
-			case "add_point": include_once("add_point.php");
+			case "add_teacher":
+				include_once("add_teacher.php");
 				break;
-			case "edit_point": include_once("edit_point.php");
+			case "edit_teacher":
+				include_once("edit_teacher.php");
 				break;
 
-			case "user": include_once("user.php");
-			break;
-			case "add-user": include_once("add_user.php");
-			break;
-			case "edit-user": include_once("edit_user.php");
-			break;
+			case "subject":
+				include_once("subject.php");
+				break;
+			case "add_subject":
+				include_once("add_subject.php");
+				break;
 
-			case "student": include_once("student.php");
-			break;
-			case "add-student": include_once("add_student.php");
-			break;
-			case "edit-student": include_once("edit_student.php");
-			break;
+			case "class":
+				include_once("class.php");
+				break;
+			case "add_class":
+				include_once("add_class.php");
+				break;
+			case "edit_class":
+				include_once("edit_class.php");
+				break;
 
-			case "schedue": include_once("schedue.php");
-			break;
-			case "add_schedue": include_once("add_schedue.php");
-			break;
+			case "semester":
+				include_once("semester.php");
+				break;
+			case "add_semester":
+				include_once("add_semester.php");
+				break;
 
-			case "category": include_once("category.php");
-			break;
-			case "add-category": include_once("add_category.php");
-			break;
-			case "edit-category": include_once("edit_category.php");
-			break;
+			case "point":
+				include_once("point.php");
+				break;
+			case "add_point":
+				include_once("add_point.php");
+				break;
+			case "edit_point":
+				include_once("edit_point.php");
+				break;
 
-			case "comment": include_once("comment.php");
-			break;
-			
+			case "user":
+				include_once("user.php");
+				break;
+			case "add-user":
+				include_once("add_user.php");
+				break;
+			case "edit-user":
+				include_once("edit_user.php");
+				break;
+
+			case "student":
+				include_once("student.php");
+				break;
+			case "add-student":
+				include_once("add_student.php");
+				break;
+			case "edit-student":
+				include_once("edit_student.php");
+				break;
+
+			case "schedue":
+				include_once("schedue.php");
+				break;
+			case "add_schedue":
+				include_once("add_schedue.php");
+				break;
+
+			case "category":
+				include_once("category.php");
+				break;
+			case "add-category":
+				include_once("add_category.php");
+				break;
+			case "edit-category":
+				include_once("edit_category.php");
+				break;
+
+			case "comment":
+				include_once("comment.php");
+				break;
 		}
-
 	} else {
 		include_once("dashboard.php");
 	}
 
 	?>
 	<script>
-	  function conf(){
-	    return confirm("Are you sure delete?");
-	  }
+		function conf() {
+			return confirm("Are you sure delete?");
+		}
 	</script>
 	<script src="js/jquery-1.11.1.min.js"></script>
 	<script src="js/bootstrap-table.js"></script>
--- query/vin_school.sql
+++ query/vin_school.sql
@@ -1,242 +1,374 @@
 -- phpMyAdmin SQL Dump
+
 -- version 5.2.0
+
 -- https://www.phpmyadmin.net/
+
 --
+
 -- Host: 127.0.0.1
+
 -- Generation Time: Mar 02, 2023 at 07:13 PM
+
 -- Server version: 10.4.27-MariaDB
+
 -- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+
 START TRANSACTION;
+
 SET time_zone = "+00:00";
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+;
+
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */
+
+;
+
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */
+
+;
+
+/*!40101 SET NAMES utf8mb4 */
+
+;
 
 --
+
 -- Database: `vin_school`
+
 --
 
 -- --------------------------------------------------------
 
 --
+
 -- Table structure for table `class`
+
 --
 
-CREATE TABLE `class` (
-  `class_id` int(11) NOT NULL,
-  `class_name` varchar(255) NOT NULL,
-  `teacher_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+CREATE TABLE
+    `class` (
+        `class_id` int(11) NOT NULL,
+        `class_name` varchar(255) NOT NULL,
+        `teacher_id` int(11) NOT NULL
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_general_ci;
 
 -- --------------------------------------------------------
 
 --
+
 -- Table structure for table `point_student`
+
 --
 
-CREATE TABLE `point_student` (
-  `point_id` int(11) NOT NULL,
-  `point_number` double NOT NULL,
-  `point_student_id` int(11) NOT NULL,
-  `semester_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+CREATE TABLE
+    `point_student` (
+        `point_id` int(11) NOT NULL,
+        `point_number` double NOT NULL,
+        `point_student_id` int(11) NOT NULL,
+        `semester_id` int(11) NOT NULL,
+        `subject_id` int(11) NOT NULL
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
+
 -- Table structure for table `schedue`
+
 --
 
-CREATE TABLE `schedue` (
-  `id` int(11) NOT NULL,
-  `teacher_id` int(11) NOT NULL,
-  `day` int(11) NOT NULL,
-  `kip` int(11) NOT NULL,
-  `subject_id` int(11) NOT NULL,
-  `class_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+CREATE TABLE
+    `schedue` (
+        `id` int(11) NOT NULL,
+        `teacher_id` int(11) NOT NULL,
+        `day` int(11) NOT NULL,
+        `kip` int(11) NOT NULL,
+        `subject_id` int(11) NOT NULL,
+        `class_id` int(11) NOT NULL
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
+
 -- Table structure for table `semester`
+
 --
 
-CREATE TABLE `semester` (
-  `semester_id` int(11) NOT NULL,
-  `semester_name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+CREATE TABLE
+    `semester` (
+        `semester_id` int(11) NOT NULL,
+        `semester_name` varchar(255) NOT NULL
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_general_ci;
 
 -- --------------------------------------------------------
 
 --
+
 -- Table structure for table `student`
+
 --
 
-CREATE TABLE `student` (
-  `student_id` int(11) NOT NULL,
-  `student_name` varchar(255) NOT NULL,
-  `student_phone` varchar(255) NOT NULL,
-  `student_address` varchar(255) NOT NULL,
-  `class_id` int(11) NOT NULL,
-  `student_birth_day` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+CREATE TABLE
+    `student` (
+        `student_id` int(11) NOT NULL,
+        `student_name` varchar(255) NOT NULL,
+        `student_phone` varchar(255) NOT NULL,
+        `student_address` varchar(255) NOT NULL,
+        `class_id` int(11) NOT NULL,
+        `student_birth_day` varchar(255) NOT NULL
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
+
 -- Table structure for table `subject`
+
 --
 
-CREATE TABLE `subject` (
-  `subject_id` int(11) NOT NULL,
-  `subject_name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+CREATE TABLE
+    `subject` (
+        `subject_id` int(11) NOT NULL,
+        `subject_name` varchar(255) NOT NULL
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_general_ci;
 
 -- --------------------------------------------------------
 
 --
+
 -- Table structure for table `teacher`
+
 --
 
-CREATE TABLE `teacher` (
-  `teacher_id` int(11) NOT NULL,
-  `teacher_name` varchar(255) NOT NULL,
-  `teacher_email` varchar(255) NOT NULL,
-  `teacher_phone` varchar(255) NOT NULL,
-  `class_id` int(11) NOT NULL,
-  `teacher_address` varchar(255) NOT NULL,
-  `teacher_birth_day` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+CREATE TABLE
+    `teacher` (
+        `teacher_id` int(11) NOT NULL,
+        `teacher_name` varchar(255) NOT NULL,
+        `teacher_email` varchar(255) NOT NULL,
+        `teacher_phone` varchar(255) NOT NULL,
+        `class_id` int(11) NOT NULL,
+        `teacher_address` varchar(255) NOT NULL,
+        `teacher_birth_day` varchar(255) NOT NULL
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
+
 -- Table structure for table `user`
---
-
-CREATE TABLE `user` (
-  `user_id` int(11) NOT NULL,
-  `user_full` varchar(255) NOT NULL,
-  `user_mail` varchar(255) NOT NULL,
-  `user_pass` varchar(255) NOT NULL,
-  `user_level` int(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
+
+CREATE TABLE
+    `user` (
+        `user_id` int(11) NOT NULL,
+        `user_full` varchar(255) NOT NULL,
+        `user_mail` varchar(255) NOT NULL,
+        `user_pass` varchar(255) NOT NULL,
+        `user_level` int(1) NOT NULL
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_unicode_ci;
+
+--
+
 -- Dumping data for table `user`
---
-
-INSERT INTO `user` (`user_id`, `user_full`, `user_mail`, `user_pass`, `user_level`) VALUES
-(2, 'Admin', 'admin@gmail.com', '123456', 1);
 
 --
+
+INSERT INTO
+    `user` (
+        `user_id`,
+        `user_full`,
+        `user_mail`,
+        `user_pass`,
+        `user_level`
+    )
+VALUES (
+        2,
+        'Admin',
+        'admin@gmail.com',
+        '123456',
+        1
+    );
+
+--
+
 -- Indexes for dumped tables
+
 --
 
 --
+
 -- Indexes for table `class`
---
-ALTER TABLE `class`
-  ADD PRIMARY KEY (`class_id`),
-  ADD KEY `teacher_id` (`teacher_id`);
 
 --
+
+ALTER TABLE `class`
+ADD PRIMARY KEY (`class_id`),
+ADD
+    KEY `teacher_id` (`teacher_id`);
+
+--
+
 -- Indexes for table `point_student`
---
-ALTER TABLE `point_student`
-  ADD PRIMARY KEY (`point_id`),
-  ADD KEY `semester_id` (`semester_id`),
-  ADD KEY `student_id` (`point_student_id`);
 
 --
+
+ALTER TABLE `point_student`
+ADD PRIMARY KEY (`point_id`),
+ADD
+    KEY `semester_id` (`semester_id`),
+ADD
+    KEY `student_id` (`point_student_id`);
+
+--
+
 -- Indexes for table `schedue`
---
-ALTER TABLE `schedue`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `subject_id` (`subject_id`),
-  ADD KEY `class_id` (`class_id`),
-  ADD KEY `teacher_id` (`teacher_id`);
 
 --
+
+ALTER TABLE `schedue`
+ADD PRIMARY KEY (`id`),
+ADD
+    KEY `subject_id` (`subject_id`),
+ADD
+    KEY `class_id` (`class_id`),
+ADD
+    KEY `teacher_id` (`teacher_id`);
+
+--
+
 -- Indexes for table `semester`
---
-ALTER TABLE `semester`
-  ADD PRIMARY KEY (`semester_id`);
 
 --
+
+ALTER TABLE `semester` ADD PRIMARY KEY (`semester_id`);
+
+--
+
 -- Indexes for table `student`
---
-ALTER TABLE `student`
-  ADD PRIMARY KEY (`student_id`),
-  ADD UNIQUE KEY `student_phone` (`student_phone`),
-  ADD KEY `class_id` (`class_id`);
 
 --
+
+ALTER TABLE `student`
+ADD
+    PRIMARY KEY (`student_id`),
+ADD
+    UNIQUE KEY `student_phone` (`student_phone`),
+ADD KEY `class_id` (`class_id`);
+
+--
+
 -- Indexes for table `subject`
---
-ALTER TABLE `subject`
-  ADD PRIMARY KEY (`subject_id`);
 
 --
+
+ALTER TABLE `subject` ADD PRIMARY KEY (`subject_id`);
+
+--
+
 -- Indexes for table `teacher`
---
-ALTER TABLE `teacher`
-  ADD PRIMARY KEY (`teacher_id`),
-  ADD UNIQUE KEY `teacher_email` (`teacher_email`),
-  ADD UNIQUE KEY `teacher_phone` (`teacher_phone`),
-  ADD KEY `FK_teacher_class` (`class_id`);
 
 --
+
+ALTER TABLE `teacher`
+ADD
+    PRIMARY KEY (`teacher_id`),
+ADD
+    UNIQUE KEY `teacher_email` (`teacher_email`),
+ADD
+    UNIQUE KEY `teacher_phone` (`teacher_phone`),
+ADD
+    KEY `FK_teacher_class` (`class_id`);
+
+--
+
 -- Indexes for table `user`
+
 --
+
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`user_id`),
-  ADD UNIQUE KEY `user_mail` (`user_mail`);
+ADD PRIMARY KEY (`user_id`),
+ADD
+    UNIQUE KEY `user_mail` (`user_mail`);
 
 --
+
 -- Constraints for dumped tables
+
 --
 
 --
+
 -- Constraints for table `class`
+
 --
+
 ALTER TABLE `class`
-  ADD CONSTRAINT `class_ibfk_1` FOREIGN KEY (`teacher_id`) REFERENCES `teacher` (`teacher_id`);
+ADD
+    CONSTRAINT `class_ibfk_1` FOREIGN KEY (`teacher_id`) REFERENCES `teacher` (`teacher_id`);
 
 --
+
 -- Constraints for table `point_student`
+
 --
+
 ALTER TABLE `point_student`
-  ADD CONSTRAINT `point_student_ibfk_1` FOREIGN KEY (`semester_id`) REFERENCES `semester` (`semester_id`),
-  ADD CONSTRAINT `point_student_ibfk_2` FOREIGN KEY (`point_student_id`) REFERENCES `student` (`student_id`);
+ADD
+    CONSTRAINT `point_student_ibfk_1` FOREIGN KEY (`semester_id`) REFERENCES `semester` (`semester_id`),
+ADD
+    CONSTRAINT `point_student_ibfk_2` FOREIGN KEY (`point_student_id`) REFERENCES `student` (`student_id`);
 
 --
+
 -- Constraints for table `schedue`
+
 --
+
 ALTER TABLE `schedue`
-  ADD CONSTRAINT `schedue_ibfk_1` FOREIGN KEY (`subject_id`) REFERENCES `subject` (`subject_id`),
-  ADD CONSTRAINT `schedue_ibfk_2` FOREIGN KEY (`class_id`) REFERENCES `class` (`class_id`),
-  ADD CONSTRAINT `schedue_ibfk_3` FOREIGN KEY (`teacher_id`) REFERENCES `teacher` (`teacher_id`);
+ADD
+    CONSTRAINT `schedue_ibfk_1` FOREIGN KEY (`subject_id`) REFERENCES `subject` (`subject_id`),
+ADD
+    CONSTRAINT `schedue_ibfk_2` FOREIGN KEY (`class_id`) REFERENCES `class` (`class_id`),
+ADD
+    CONSTRAINT `schedue_ibfk_3` FOREIGN KEY (`teacher_id`) REFERENCES `teacher` (`teacher_id`);
 
 --
+
 -- Constraints for table `student`
---
-ALTER TABLE `student`
-  ADD CONSTRAINT `student_ibfk_1` FOREIGN KEY (`class_id`) REFERENCES `class` (`class_id`);
 
 --
--- Constraints for table `teacher`
+
+ALTER TABLE `student`
+ADD
+    CONSTRAINT `student_ibfk_1` FOREIGN KEY (`class_id`) REFERENCES `class` (`class_id`);
+
 --
+
+-- Constraints for table `teacher`
+
+--
+
 ALTER TABLE `teacher`
-  ADD CONSTRAINT `FK_teacher_class` FOREIGN KEY (`class_id`) REFERENCES `class` (`class_id`);
+ADD
+    CONSTRAINT `FK_teacher_class` FOREIGN KEY (`class_id`) REFERENCES `class` (`class_id`);
+
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */
+
+;
+
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */
+
+;
+
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */
+
+;
